36026a59538700f24474ac91eef7f7e2ad817de4
Candida Rosa Paraizo <krisvran24@gmail.com>
2025-06-04 02:32:36 +0000

    Added Form Components

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,194 +1,116 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { generateClient } from 'aws-amplify/api';
-import { useBreakpointValue } from '@aws-amplify/ui-react';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
-import Navigation from './navigation';
-import AdminControls from './adminControls';
-import ProductMediumCollection from './productMediumCollection';
+import ProductCreateForm from './ProductCreateForm';
+import ProductUpdateForm from './ProductUpdateForm';
+import CategoryCreateForm from './CategoryCreateForm';
+import CategoryUpdateForm from './CategoryUpdateForm';
 
-import AppLayout from '@cloudscape-design/components/app-layout';
-import Container from '@cloudscape-design/components/container';
-import Header from '@cloudscape-design/components/header';
-import ContentLayout from '@cloudscape-design/components/content-layout';
-import Alert from '@cloudscape-design/components/alert';
+import Modal from '@cloudscape-design/components/modal';
+import Button from '@cloudscape-design/components/button';
 import SpaceBetween from '@cloudscape-design/components/space-between';
-import Multiselect from '@cloudscape-design/components/multiselect';
 
-/** @type {ReturnType<typeof generateClient<import('../../amplify/data/resource').Schema>} */
-const client = generateClient();
-
-export default function Category() {
-  // this component is used when something hits /category/{name}
-  // name is used to represent the category we are viewing
-  const { name } = useParams();
-  const [prevName, setPrevName] = useState(''); //prevName allows us to see if the category has changed
-  const [products, setProducts] = useState([]); //products is a list of the products to display filtered down
-  const [allStyles, setAllStyles] = useState([]); //allStyles is a list of all the styles for building the filters
-  const [filteredStyles, setFilteredStyles] = useState([]); //filteredStyles is a list of styles that are selected for filtering
-  const [, setCategoryId] = useState(''); //categoryId for use in subscription filtering
-  const [, setFilterStyleList] = useState([]); // filter list with style labels for use in subscription filtering
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertStatus, setAlertStatus] = useState({
-    type: 'success',
-    message: 'Success!',
+export default function AdminControls(props) {
+  const {
+    productButtonText,
+    categoryButtonText,
+    showNewProduct,
+    showNewCategory,
+    alertHandler,
+    product
+  } = props;
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [user, setUser] = useState(null);
+  const [modalConfig, setModalConfig] = useState({
+    showProduct: true,
+    type: 'Create',
   });
-  
-  useEffect(() => {
-    // Query for categories to get all the styles that make up that category
-    // so we can build the filter display
-    async function queryCategory() {
-      try {
-        const { data: categories, errors } = await client.models.Category.list({
-          filter: { name: { eq: name } },
-        });
-        if (categories) {
-          setAllStyles(Object.values(categories[0].styles)); 
-          setCategoryId(categories[0].id);
-          return categories[0].id;
-        }
-      } catch (error) {
-        console.log("Error retrieving category", error);
-      }
-    }
-
-    // queryProduct is used to query the products and filter by category and filtered again
-    // based on any selected filters
-    async function queryProduct() {
-      try {
-        const { data: categories, errors } = await client.models.Category.list({
-          filter: { name: { eq: name } },
-        });
-        if (categories) {
-          const { data: products, errors } = await client.models.Product.list({
-            limit: 3000,
-            filter: { categoryProductsId: { eq: categories[0].id } },
-          });
-          
-          const filterList = filteredStyles.map((style) => {
-            return style.label;
-          });
-          setFilterStyleList(filterList);
-
-          // Filter the results if a style is selected
-          setProducts(
-            products.filter(
-              (product) =>
-                filterList.includes(product.style) ||
-                filteredStyles.length === 0
-            )
-          );
-        }
-      } catch (error) {
-        console.log("Error retrieving posts", error);
-      }
-    }
-
-    if (prevName !== name || prevName !== "") {
-      if (prevName !== name) {
-        setFilteredStyles([]);
-      }
-      queryCategory();
-      setPrevName(name);
-      queryProduct();
-    }
-  }, [name, filteredStyles, prevName]);
 
   useEffect(() => {
-    if (showAlert) {
-      const timeId = setTimeout(() => {
-        setShowAlert(false);
-      }, 5000);
-
-      return () => {
-        clearTimeout(timeId);
-      };
+    async function isAdminUser() {
+      try {
+        const user = await fetchAuthSession();
+        setIsAdmin(user.tokens.accessToken.payload["cognito:groups"].includes('Admin'));
+        setUser(user);
+      } catch (err) {
+        setIsAdmin(false);
+        setUser(null);
+      }
     }
-  }, [showAlert]);
+    isAdminUser();
+  }, []);
 
-  const alertHandler = (alertStatus) => {
-    setAlertStatus(alertStatus);
-    setShowAlert(true);
+  const handleAction = (type, e) => {
+    e.preventDefault();
+    setModalConfig({
+      showProduct: type === 'product', 
+      type: productButtonText.includes("Edit") ? 'Update' : 'Create'
+    });
+    setVisible(true);
   };
 
-  // overrides for our collection
-  // Change the style of the collection to be a list on smaller screen formats and grid
-  // on larger formats. Also change the items displayed depending on the screen size
-  const overrides = {
-    ProductMediumCollection: {
-      type: useBreakpointValue({
-        small: "list",
-        base: "list",
-        medium: "grid",
-      }),
-      itemsPerPage: useBreakpointValue({
-        base: "5",
-        small: "5",
-        medium: "10",
-        large: "10",
-        xl: "10",
-      }),
-    },
+  const handleSuccess = (fields) => {
+    alertHandler({
+      type: 'success',
+      message: 'Successfully submitted new item!'
+    });
+    setVisible(false);
+  };
+
+  const handleError = (fields, msg) => {
+    alertHandler({
+      type: 'error',
+      message: `There was an issue submitting your new item: ${msg}`
+    });
+    setVisible(false);
   };
 
   return (
-    <AppLayout
-      navigation={<Navigation activeHref="/" />}
-      toolsHide={true}
-      navigationWidth={200}
-      minContentWidth={1000}
-      maxContentWidth={2000}
-      content={
-        <ContentLayout
-          header={
-            <SpaceBetween size="m">
-              <Header
-                variant="h1"
-                actions={
-                  <AdminControls
-                    showNewProduct={true}
-                    showNewCategory={false}
-                    productButtonText={"New Product"}
-                    categoryButtonText={"New Category"}
-                    alertHandler={alertHandler}
-                    product={null}
-                  />
-                }
-              >
-                {`Products - ${name}`}
-              </Header>
-              {showAlert
-              ? <Alert type={alertStatus.type}>{alertStatus.message}</Alert>
-              : null
-              }
-              <Multiselect
-                selectedOptions={filteredStyles}
-                onChange={({detail}) => {
-                  setFilteredStyles(detail.selectedOptions)
-                }}
-                deselectAriaLabel={e => `Remove ${e.label}`}
-                options={
-                  allStyles.map((style) => {
-                    return {
-                      label: style,
-                      value: style
-                    }
-                  })
-                }
-                placeholder="Select styles"
-                selectedAriaLabel="Selected"
-              />
-            </SpaceBetween>
-          }
-        >
-          <Container>
-            <ProductMediumCollection
-              items={products}
-              overrides={overrides}
-            />
-          </Container>
-        </ContentLayout>
+    <>
+      {(user !== undefined && isAdmin)
+      ? <>
+          <SpaceBetween direction="horizontal" size="xs">
+            {showNewProduct
+            ? <Button variant="primary" onClick={(e) => handleAction("product", e)}>
+                {productButtonText}
+              </Button>
+            : null
+            }
+            {showNewCategory
+            ? <Button variant="primary" onClick={(e) => handleAction("category", e)}>
+                {categoryButtonText}
+              </Button>
+            : null
+            }
+          </SpaceBetween>
+          <Modal
+            onDismiss={() => setVisible(false)}
+            visible={visible}
+            closeAriaLabel="Close modal"
+            header={modalConfig.showProduct ? productButtonText : categoryButtonText}
+            modalRoot={document.getElementById('top-nav')}
+          >
+            {modalConfig.showProduct && modalConfig.type === 'Create'
+            ? <ProductCreateForm onSuccess={handleSuccess} onError={handleError}/>
+            : null
+            }
+            {modalConfig.showProduct && modalConfig.type === 'Update'
+            ? <ProductUpdateForm product={product} onSuccess={handleSuccess} onError={handleError}/>
+            : null
+            }
+            {!modalConfig.showProduct && modalConfig.type === 'Create'
+            ? <CategoryCreateForm onSuccess={handleSuccess} onError={handleError} />
+            : null
+            }
+            {!modalConfig.showProduct && modalConfig.type === 'Update'
+            ? <CategoryUpdateForm onSuccess={handleSuccess} onError={handleError} />
+            : null
+            }
+          </Modal>
+        </>
+      : <></>
       }
-    />
+    </>
   );
 }
